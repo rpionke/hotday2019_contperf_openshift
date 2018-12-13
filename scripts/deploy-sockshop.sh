03519aa7b83949b1e3932ef6154c876da23f87fb
Florian Bacher <bacherfl@gmail.com>
2018-12-13 13:38:28 +0100

    Update deploy-sockshop.sh

--- scripts/deploy-sockshop.sh
+++ scripts/deploy-sockshop.sh
@@ -13,8 +13,8 @@ oc apply -f production/
 cd ../carts-db
 oc apply -f carts-db.yml
 
-#cd ../catalogue-db
-#oc apply -f catalogue-db.yml
+cd ../catalogue-db
+oc apply -f catalogue-db.yml
 
 cd ../orders-db
 oc apply -f orders-db.yml
