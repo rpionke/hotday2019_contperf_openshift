b6efa317a8fe642fe2b1fbb8c25d6e4dae4fb7a0
Florian Bacher <bacherfl@gmail.com>
2018-12-14 13:28:28 +0100

    use redhat registry container for mongodb

---
+++ scripts/install-istio.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export MASTER_URL_TMP=$(oc config view -o=json | jq -r '.["current-context"]' | sed -e 's/[^\/]*\///')
+IFS=':' read -ra MASTER_URL <<< "$MASTER_URL_TMP"
+
+oc new-project istio-operator
+oc new-app -f https://raw.githubusercontent.com/Maistra/openshift-ansible/maistra-0.3/istio/istio_product_operator_template.yaml --param OPENSHIFT_ISTIO_MASTER_PUBLIC_URL=$MASTER_URL --param OPENSHIFT_RELEASE=v3.11.16
+
+oc create -f ../manifests/istio/istio-installation.yml
+
+oc project istio-system
+oc create -f ../manifests/istio/istio-gateway.yml
+
+oc label namespace production istio-injection=enabled
+oc adm policy add-scc-to-user anyuid -z default -n production
+oc adm policy add-scc-to-user privileged -z default -n production
