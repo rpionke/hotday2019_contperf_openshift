b2ce29c22751d987f79dcadaf07ff7c2bcdf1d58
Florian Bacher <bacherfl@gmail.com>
2018-12-17 09:25:04 +0100

    fixed istio settings

--- scripts/install-istio.sh
+++ scripts/install-istio.sh
@@ -10,6 +10,9 @@ oc new-app -f https://raw.githubusercontent.com/Maistra/openshift-ansible/maistr
 
 oc create -f ../manifests/istio/istio-installation.yml
 
+# wait for the istio installation to complete - usually takes quite some time
+sleep 250
+
 oc project istio-system
 oc create -f ../manifests/istio/istio-gateway.yml
 
