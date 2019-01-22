c6050e72633dc07077af73436d361f19f78aff9e
Andreas Grabner <grabnerandi@gmx.at>
2019-01-22 13:26:05 +0100

    added docker.io reference

--- docker-jenkins-neotys/docker_build_push.sh
+++ docker-jenkins-neotys/docker_build_push.sh
@@ -2,5 +2,5 @@
 
 # Builds Jenkins Image and pushes to docker hub
 docker build --no-cache -t jenkinshotday2019_neotys .
-docker tag jenkinshotday2019_neotys grabnerandi/jenkinshotday2019_neotys:0.1
-docker push grabnerandi/jenkinshotday2019_neotys:0.1
+docker tag jenkinshotday2019_neotys docker.io/grabnerandi/jenkinshotday2019_neotys:0.1
+docker push docker.io/grabnerandi/jenkinshotday2019_neotys:0.1
--- docker-jenkins/docker_build_push.sh
+++ docker-jenkins/docker_build_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 # Builds Jenkins Image and pushes to docker hub
 docker build --no-cache -t jenkinshotday2019 .
-docker tag jenkinshotday2019 grabnerandi/jenkinshotday2019:0.1
-docker push grabnerandi/jenkinshotday2019:0.1
+docker tag jenkinshotday2019 docker.io/grabnerandi/jenkinshotday2019:0.1
+docker push docker.io/grabnerandi/jenkinshotday2019:0.1
