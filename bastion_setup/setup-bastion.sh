2c694ac8adebca6b99984887ebd0802f4f1a11d0
Andreas Grabner <grabnerandi@gmx.at>
2019-01-24 13:03:46 +0100

    fixing bastion setup

--- bastion_setup/setup-bastion.sh
+++ bastion_setup/setup-bastion.sh
@@ -12,20 +12,21 @@ mv jq /usr/bin/
 rm -rf hotday2019_contperf_openshift
 git clone https://github.com/grabnerandi/hotday2019_contperf_openshift
 
-STUDENTID=$1
+STUDENTIX=$1
 # now lets update creds with our studentid
-if [[ $STUDENTID = '' ]]
+if [[ $STUDENTIX = '' ]]
 then
-  STUDENTID=0
+  STUDENTIX=0
 fi
 
-echo "Setting up StudentID=$STUDENTID"
+echo "Setting up StudentID=$STUDENTIX"
 echo $(pwd)
 
-export DT_TENANT_ID=$(cat ./setup-bastion-init.json | jq -r ".students[$STUDENTID].tenant")
-export DT_PAAS_TOKEN=$(cat ./setup-bastion-init.json | jq -r ".students[$STUDENTID].paastoken")
-export DT_API_TOKEN=$(cat ./setup-bastion-init.json | jq -r ".students[$STUDENTID].apitoken")
-export NL_WEB_API_KEY=$(cat ./setup-bastion-init.json | jq -r ".students[$STUDENTID].nltoken")
+export DT_TENANT_ID=$(cat ./setup-bastion-init.json | jq -r ".students[$STUDENTIX].tenant")
+export DT_PAAS_TOKEN=$(cat ./setup-bastion-init.json | jq -r ".students[$STUDENTIX].paastoken")
+export DT_API_TOKEN=$(cat ./setup-bastion-init.json | jq -r ".students[$STUDENTIX].apitoken")
+export NL_WEB_API_KEY=$(cat ./setup-bastion-init.json | jq -r ".students[$STUDENTIX].nltoken")
+export STUDENT_ID=$(cat ./setup-bastion-init.json | jq -r ".students[$STUDENTIX].studentid")
 
 echo $DT_TENANT_ID
 echo $DT_PAAS_TOKEN
@@ -36,3 +37,7 @@ sed -i 's/DTENV_PLACEHOLDER/'"$DT_TENANT_ID"'/' ./hotday2019_contperf_openshift/
 sed -i 's/DTAPI_PLACEHOLDER/'"$DT_PAAS_TOKEN"'/' ./hotday2019_contperf_openshift/scripts/creds.sh
 sed -i 's/DTPAPI_PLACEHOLDER/'"$DT_API_TOKEN"'/' ./hotday2019_contperf_openshift/scripts/creds.sh
 sed -i 's/NLAPI_PLACEHOLDER/'"$NL_WEB_API_KEY"'/' ./hotday2019_contperf_openshift/scripts/creds.sh
+
+# lets login to oc
+# TODO - REPLACE username & PWD with real values!!
+oc login master.$STUDENT_ID.ocpworkshop.dtinnovationlab.net -u USERNAME_PLACEHOLDER -p PWD_PLACEHOLDER
