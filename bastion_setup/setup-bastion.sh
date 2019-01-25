5db1c72c5179c7bf7ce7af5d469438d1292c9585
Andreas Grabner <grabnerandi@gmx.at>
2019-01-25 12:00:37 +0100

    additonal bastion update steps

--- bastion_setup/setup-bastion.sh
+++ bastion_setup/setup-bastion.sh
@@ -2,11 +2,11 @@
 
 # Will make sure the bastion host is setup with all necessary tools such as hub and j2
 sudo wget https://github.com/github/hub/releases/download/v2.6.0/hub-linux-amd64-2.6.0.tgz
-tar -xzf hub-linux-amd64-2.6.0.tgz
-sudo cp hub-linux-amd64-2.6.0/bin/hub /bin/
+tar -xzf ~/hub-linux-amd64-2.6.0.tgz
+sudo cp ~/hub-linux-amd64-2.6.0/bin/hub /bin/
 wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O jq
-chmod +x jq
-mv jq /usr/bin/
+chmod +x ~/jq
+sudo mv ~/jq /usr/bin/
 
 # clone workshop repo - first remove the dir in case it was already there
 rm -rf hotday2019_contperf_openshift
@@ -34,10 +34,10 @@ echo $DT_API_TOKEN
 echo $NL_WEB_API_KEY
 
 sed -i 's/DTENV_PLACEHOLDER/'"$DT_TENANT_ID"'/' ./hotday2019_contperf_openshift/scripts/creds.sh
-sed -i 's/DTAPI_PLACEHOLDER/'"$DT_PAAS_TOKEN"'/' ./hotday2019_contperf_openshift/scripts/creds.sh
-sed -i 's/DTPAPI_PLACEHOLDER/'"$DT_API_TOKEN"'/' ./hotday2019_contperf_openshift/scripts/creds.sh
+sed -i 's/DTPAPI_PLACEHOLDER/'"$DT_PAAS_TOKEN"'/' ./hotday2019_contperf_openshift/scripts/creds.sh
+sed -i 's/DTAPI_PLACEHOLDER/'"$DT_API_TOKEN"'/' ./hotday2019_contperf_openshift/scripts/creds.sh
 sed -i 's/NLAPI_PLACEHOLDER/'"$NL_WEB_API_KEY"'/' ./hotday2019_contperf_openshift/scripts/creds.sh
 
 # lets login to oc
 # TODO - REPLACE username & PWD with real values!!
-oc login master.$STUDENT_ID.ocpworkshop.dtinnovationlab.net -u USERNAME_PLACEHOLDER -p PWD_PLACEHOLDER
+oc login https://master1 --insecure-skip-tls-verify=true -u $2 -p $3
--- scripts/cleanup_workshop.sh
+++ scripts/cleanup_workshop.sh
@@ -6,7 +6,7 @@ oc delete -f ../manifests/oc-scc-hostpath.yml
 oc delete -f ../manifests/k8s-jenkins-rbac.yml
 oc delete -f ../manifests/k8s-jenkins-neotys-rbac.yml
 oc delete -f ../manifests/istio/istio-gateway.yml
-oc delete -f ../manifests/isito/istio-installation.yml
+oc delete -f ../manifests/istio/istio-installation.yml
 oc adm policy remove-scc-from-user anyuid -z default -n production
 oc adm policy remove-scc-from-user privileged -z default -n production
 oc delete projects cicd cicd-neotys dev staging production sockshop-registry istio-system istio-operator
